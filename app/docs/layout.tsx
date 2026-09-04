1330e59952fb32a452f61f9166ef4bcb89ffcb88
Maximus7474 <the.maximus.7474@gmail.com>
2026-09-04 22:59:55 +0200

    fix(docs): page metadata

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -1,6 +1,21 @@
 import { source } from "@/lib/source";
 import { DocsLayout } from "fumadocs-ui/layouts/docs";
 import { baseOptions, navLinks } from "@/lib/layout.shared";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "fxManager Docs",
+  description:
+    "Documentation to integrate, use and install fxManager",
+  icons: {
+    icon: [
+      {
+        url: "/icon.svg",
+        type: "image/svg+xml",
+      },
+    ],
+  },
+};
 
 export default function Layout({ children }: LayoutProps<"/docs">) {
   const filteredLinks = navLinks.filter((link) => link.url !== "/docs");
